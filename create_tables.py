cdc7bdf24b07bc01ffa41a7d7161b11f84be860a
Tania <tvasilikioti@babbel.com>
2020-08-02 16:06:17 +0200

    add exception handling and logger

--- create_tables.py
+++ create_tables.py
@@ -4,12 +4,14 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """Executes SQL queries from sql_queries to drop tables"""
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """Executes SQL queries from sql_queries to create needed tables"""
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
